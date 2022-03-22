4ad58c606b27a2650e36eb91576b03e476ad8a09
Til Blechschmidt <til@blechschmidt.de>
2022-03-23 00:11:28 +0100

    Initial Commit

---
+++ Shared/HobbsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbsCalculatorApp.swift
+//  Shared
+//
+//  Created by Til Blechschmidt on 23.03.22.
+//
+
+import SwiftUI
+
+@main
+struct HobbsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
