26415e7711e747b19c8e4e5171356cb276e04f5c
Til Blechschmidt <til@blechschmidt.de>
2022-11-11 14:54:08 +0000

    Simplify TimeInput to use a standard TextInput

--- Shared/Views/FlightCreation/Helpers/TimeInput.swift
+++ Shared/Views/FlightCreation/Helpers/TimeInput.swift
@@ -1,21 +1,14 @@
 //
-//  TimeInput.swift
+//  SimpleTimeInput.swift
 //  HobbsCalculator
 //
-//  Created by Til Blechschmidt on 26.03.22.
+//  Created by Til Blechschmidt on 11.11.22.
 //
 
 import SwiftUI
 
 struct TimeInput: View {
-    @FocusState private var inputFocused: Bool
-    @FocusState private var fakeInputFocused: Bool
-
-    @State var hours: String
-    @State var minutes: String
-
-    @State var ignoreChanges: Bool = false
-    @State var useNumpad: Bool = false
+    @State var text: String
 
     let parser: TimeParser
     let value: Binding<TimeInterval?>
@@ -26,107 +19,24 @@ struct TimeInput: View {
 
         if let currentValue = value.wrappedValue {
             let (hours, minutes) = parser.split(currentValue)
-            self.hours = String(format: "%02d", hours)
-            self.minutes = String(format: "%02d", minutes)
+            _text = State(wrappedValue: String(format: "%02d:%02d", hours, minutes))
         } else {
-            self.hours = "00"
-            self.minutes = "00"
+            _text = State(wrappedValue: "")
         }
     }
 
     var body: some View {
-        HStack(alignment: .center, spacing: 3) {
-            TextField("00", text: $hours)
-                .fixedSize()
-                .monospacedDigit()
-                .multilineTextAlignment(.trailing)
-                .disabled(true)
-            Text(":")
-            TextField("00", text: $minutes)
-                .fixedSize()
-                .monospacedDigit()
-                .multilineTextAlignment(.leading)
-                .focused($inputFocused)
-                .keyboardType(.numberPad)
-        }
-            // Updating the value in the same render cycle makes SwiftUI complain
-            // but for now it works.
-            .onChange(of: hours, perform: overflowHandling)
-            .onChange(of: minutes, perform: overflowHandling)
-            // Moves cursor to the end of the input when focused
-            .onChange(of: inputFocused) { _ in
-                if inputFocused {
-                    ignoreChanges = true
-                    let text = minutes
-                    minutes = ""
-                    DispatchQueue.main.async {
-                        minutes = text
-                        ignoreChanges = false
-                    }
-                }
-            }
-            // Moves focus from fake input to target input
-            // Workaround for a bug where a change from alphanumeric keyboard to numberPad causes the scrollview to glitch out
-            .onChange(of: fakeInputFocused) { _ in
-                if fakeInputFocused {
-                    inputFocused = true
-                }
-            }
-            .overlay {
-                GeometryReader { geometry in
-                    TextField("", text: Binding.init(get: { "" }, set: { _ in }))
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .background(.background)
-                        // We can not use .clear or 0 or any value below 0.05 because then SwiftUI just removes the focus target and the .focused will not work
-                        .opacity(0.05)
-                        .focused($fakeInputFocused)
-                }
-            }
+        TextField("00:00", text: $text)
+            .fixedSize()
+            .monospacedDigit()
+            .multilineTextAlignment(.trailing)
+            .keyboardType(.decimalPad)
+            .onChange(of: text, perform: separatorReplacement)
     }
 
-    func overflowHandling(_: String) {
-        guard !ignoreChanges else { return }
-
-        var hours = hours
-        var minutes = minutes
-
-        if hours.count + minutes.count > 10 {
-            minutes.removeLast()
-        }
-
-        hours = hours
-            .replacingOccurrences(of: ",", with: "")
-            .replacingOccurrences(of: ".", with: "")
-
-        minutes = minutes
-            .replacingOccurrences(of: ",", with: "")
-            .replacingOccurrences(of: ".", with: "")
-
-        if minutes.count > 2 {
-            let prefix = minutes.prefix(minutes.count - 2)
-            minutes.removeFirst(prefix.count)
-            hours += prefix
-        }
-
-        if minutes.count < 2 && hours.count > 0 {
-            let suffix = hours.suffix(2 - minutes.count)
-            hours.removeLast(suffix.count)
-            minutes = suffix + minutes
-        } else if minutes.count < 2 {
-            minutes = String(repeating: "0", count: 2 - minutes.count) + minutes
-        }
-
-        while hours.starts(with: "0") {
-            hours.removeFirst()
-        }
-
-        while hours.count < 2 {
-            hours = "0" + hours
-        }
-
-        self.hours = hours
-        self.minutes = minutes
-        self.value.wrappedValue = parser.parse("\(hours):\(minutes)")
+    func separatorReplacement(src: String) {
+        text = src.replacingOccurrences(of: ",", with: ":")
+        value.wrappedValue = parser.parse(text)
     }
 }
 
--- Shared/Views/TutorialView.swift
+++ Shared/Views/TutorialView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct TutorialView: View {
     var body: some View {
         Text("Coming soon, play around with the app in the meantime.")
+        // Important notes:
+        // - Flight != Trip, one trip == multiple flights
+        //   - One flight goes from engine start to shutdown
+        // - Only write down Hobbs & UTC time, nothing else is required
     }
 }
 
